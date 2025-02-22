4e37d8312759b84bb6b84261cdc0bc008bc5d46e
Prasai1488 <prasaiprithvi4@gmail.com>
2025-02-22 20:25:45 +0545

    created models and auth api test

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import express from "express";
+import authRoute from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser"; 
+
 const app = express();
+
+
+// Middleware to parse JSON bodies in incoming requests
+app.use(express.json());
+
+
+// Middleware to parse cookies in incoming requests
+app.use(cookieParser());
+
+
+// Route for authentication-related operations
+app.use("/api/auth", authRoute);
+
+
 
 // server and network port
 const port = 8090; // 4000 to 10,000
