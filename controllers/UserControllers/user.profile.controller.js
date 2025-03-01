42b2f61b3f7af50a106b4735505a58794f5eadca
Prasai1488 <prasaiprithvi4@gmail.com>
2025-03-01 15:55:30 +0545

    update user profile api

--- controllers/UserControllers/user.profile.controller.js
+++ controllers/UserControllers/user.profile.controller.js
@@ -1,6 +1,6 @@
 import prisma from "../../lib/prisma.js";
 
-//? get profile/user details
+//? get profile details of specific user by id
 export const getUser = async (req, res) => {
   const { id } = req.params;
 
@@ -26,3 +26,62 @@ export const getUser = async (req, res) => {
   }
 };
 
+
+// ? update user profile details by id
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { email, username, password, fullName, phoneNumber, avatar } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+
+    // Check if email or username is already taken (excluding the current user)
+    if (email) {
+      const existingEmail = await prisma.user.findUnique({ where: { email } });
+      if (existingEmail && existingEmail.id !== id) {
+        return res.status(400).json({ message: "Email is already in use!" });
+      }
+    }
+
+    if (username) {
+      const existingUsername = await prisma.user.findUnique({ where: { username } });
+      if (existingUsername && existingUsername.id !== id) {
+        return res.status(400).json({ message: "Username is already taken!" });
+      }
+    }
+
+    // Prepare data to be updated
+    const updatedData = { email, username, fullName, phoneNumber, avatar };
+
+    // Hash password if provided
+    if (password) {
+      updatedData.password = await bcrypt.hash(password, 10);
+    }
+
+    // Remove undefined fields
+    Object.keys(updatedData).forEach((key) => {
+      if (updatedData[key] === undefined) {
+        delete updatedData[key];
+      }
+    });
+
+    // Update user in the database
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: updatedData,
+    });
+
+    // Exclude password before sending response
+    const { password: _, ...userInfo } = updatedUser;
+
+    res.status(200).json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to update user details!" });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getUser } from "../controllers/UserControllers/user.profile.controller.js";
+import { getUser,updateUser } from "../controllers/UserControllers/user.profile.controller.js";
 import { verifyToken } from "../middleware/verify.token.js";
 import { checkUserOwnership } from "../middleware/check.user.ownership.js";
+import validateIdFromReqParams from "../middleware/validate.id.js";
 
 const router = express.Router();
 
-router.get("/get-user/:id", verifyToken, checkUserOwnership, getUser);
+router.get("/get-user/:id", validateIdFromReqParams,verifyToken, checkUserOwnership, getUser);
+router.put("/update-user/:id", validateIdFromReqParams,verifyToken, checkUserOwnership, updateUser);
 
 export default router;
