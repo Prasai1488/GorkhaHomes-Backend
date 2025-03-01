6b5ed396379980de264c67979939523e6334edfe
Prasai1488 <prasaiprithvi4@gmail.com>
2025-03-01 20:57:24 +0545

    admin user created and property approval api endpoint

---
+++ createAdmin/create.admin.js
@@ -0,0 +1,29 @@
+import prisma from "../lib/prisma.js"
+import bcrypt from "bcrypt";
+
+async function createAdminUser() {
+    const username = 'adminUser';
+    const email = 'admin@gmail.com';
+    const password = 'admin123'; 
+    const hashedPassword = await bcrypt.hash(password, 10);
+  
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+          role: 'ADMIN', 
+        },
+      });
+  
+      console.log('Admin user created:', newUser);
+    } catch (error) {
+      console.error('Error creating admin user:', error);
+    } finally {
+      await prisma.$disconnect();
+    }
+  }
+  
+  createAdminUser();
+
