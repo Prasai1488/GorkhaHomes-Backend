5ee90e2b4f80b5ce7ef74884f06f6e8be6e21289
Prasai1488 <prasaiprithvi4@gmail.com>
2025-03-08 23:17:38 +0545

    rejection post mail send

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { approvePost } from "../controllers/AdminControllers/approve.post.js";
+import { approvePost,rejectPost } from "../controllers/AdminControllers/approve.post.js";
 import { verifyToken } from "../middleware/verify.token.js";
 import validateIdFromReqParams from "../middleware/validate.id.js";
 
 const router = express.Router();
 
 router.patch("/approve-post/:id", validateIdFromReqParams,verifyToken, approvePost);
+router.patch("/reject-post/:id", validateIdFromReqParams,verifyToken, rejectPost);
 
 export default router;
