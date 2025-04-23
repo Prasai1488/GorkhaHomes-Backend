355a429e2224bdcd585723cb748e176be462366a
Prasai1488 <prasaiprithvi4@gmail.com>
2025-04-23 10:48:02 +0545

    chat controller

--- controllers/chatMessage/chat.controller.js
+++ controllers/chatMessage/chat.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../lib/prisma.js";
+import prisma from "../../lib/prisma.js";
 
 // Function to get all chats for the authenticated user
 export const getChats = async (req, res) => {
