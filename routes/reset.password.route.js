fec229303ce859d06259a5802cfb5ceeec340485
Prasai1488 <prasaiprithvi4@gmail.com>
2025-04-26 12:55:07 +0545

    forgot password feature

---
+++ routes/reset.password.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  forgotPassword,
+  resetPassword,
+  verifyResetToken,
+} from "../controllers/ResetPassword/reset.password.controller.js";
+
+const router = express.Router();
+
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+router.get("/reset-password/:token", verifyResetToken);
+
+export default router;
